2e851719a358d398d3f95a95564196e2d4574e92
JoeGun <39112190+JonathanGun@users.noreply.github.com>
2019-06-07 15:38:35 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+var clicked = 0;
+
+function add(){
+	clicked += 1;
+	document.getElementById("clicks").innerHTML = clicked;
+}
+
+function openCV() {
+	window.location.href='assets/docs/cv.pdf';
+}
+
+function openFB() {
+	window.location.href='http://www.facebook.com/jonathan.y.gunawan';
+}
+
+function openIG() {
+	window.location.href='http://www.instagram.com/jonathany1506/';
+}
